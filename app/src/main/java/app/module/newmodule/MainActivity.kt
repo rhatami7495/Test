554d11b3816467c576e261af4373655030152a7a
RezaHatami <rhatami7495@gmail.com>
2021-05-25 11:53:56 +0430

    update gradle.

--- app/src/main/java/app/module/newmodule/MainActivity.kt
+++ app/src/main/java/app/module/newmodule/MainActivity.kt
@@ -2,10 +2,14 @@ package app.module.newmodule
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import app.module.logmodule.LogModule
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        LogModule.logD("")
+
     }
 }
